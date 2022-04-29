1fd28f69f28cc786cc82f2b8cc72f30bd4c409ac
Jan Henrik Müller <janhenrikm@yahoo.de>
2022-04-29 13:26:29 +0200

    Added --no-dynamic-linker flag for static linker
    This flag is creates an INTERP program header entry. This entry
    references the dynamic linker, which is linux specific and not
    relevant for a kernel.
    
    Removing the INTERP header is not strictly neccessary, because it
    will not be interpreted by the loader in, e.g. GRUB. Still, it
    should not be there.

--- Toolchain-x86-elf.cmake
+++ Toolchain-x86-elf.cmake
@@ -7,7 +7,7 @@ SET(CMAKE_CXX_COMPILER clang++)
 SET(CMAKE_ASM_FLAGS "-target i386-none-elf ${CMAKE_ASM_FLAGS}" CACHE STRING "" FORCE)
 SET(CMAKE_C_FLAGS "-target i386-none-elf -ffreestanding ${CMAKE_C_FLAGS}" CACHE STRING "" FORCE)
 SET(CMAKE_CXX_FLAGS "-target i386-none-elf -ffreestanding -fno-exceptions -fno-rtti ${CMAKE_CXX_FLAGS}" CACHE STRING "" FORCE)
-SET(CMAKE_EXE_LINKER_FLAGS_INIT "-target i386-linux-elf -nostdlib")
+SET(CMAKE_EXE_LINKER_FLAGS_INIT "-target i386-linux-elf -nostdlib -Wl,--no-dynamic-linker")
 
 IF(APPLE)
 	SET(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -fuse-ld=lld")
